ef49aa63753856324dedc80d70115be1b04413c9
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-06-16 12:31:03 +0300

    Update User.h
    Signed-off-by: ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>

--- User.h
+++ User.h
@@ -4,19 +4,21 @@ using namespace std;
 
 class User
 {
-   const std::string _name;
-   const std::string _login;
-   const std::string _password;
+   string _name;//если сделать _name и _password константами, то в сеттерах не удастся присвоить им значение
+   const string _login;
+   string _password;
 
 public:
-	User(const std::string& name,const std:: string& login,const std:: string& password):
+	User( const string& name,const string& login,const string& password):
 	_name(name),_login(login),_password(password){}
 	auto get_name() const {return _name;}
-	void set_name(std::string& name) {name = name;}
+	auto set_name(const string& name) { _name = name; }
 	auto get_login() const{ return _login; }
 	auto get_password()const {return _password;}
-	void set_password(std::string& password) {password = _password;}
+	auto set_password( const string& password) { _password = password; }
 	
+	
+};
 	
 };
 
